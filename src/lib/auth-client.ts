fc949dc54af71f2ef770decc5e7f654e0ab3efe8
mrhujaifa <mdhujaifaislamshanto@gmail.com>
2026-03-30 15:39:10 +0600

    fix(env): env problem fixed

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,14 @@
 import { createAuthClient } from "better-auth/react";
+
+const NEXT_PUBLIC_API_BACKEND_URL = process.env
+  .NEXT_PUBLIC_API_BACKEND_URL as string;
+
+if (!NEXT_PUBLIC_API_BACKEND_URL) {
+  throw new Error("env NEXT_PUBLIC_API_BACKEND_URL not found");
+}
+
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:5000",
+  baseURL: NEXT_PUBLIC_API_BACKEND_URL,
   basePath: "/api/auth",
   fetchOptions: {
     credentials: "include",
